81537072dd74777d6514f36c50cec203536d8434
blong04 <long0961511354@gmail.com>
2025-12-11 12:12:09 +0700

    a

--- config.php
+++ config.php
@@ -1,8 +1,8 @@
 <?php
-$host = 'localhost';
-$db   = 'guestbook_db';
-$user = 'root';
-$pass = ''; // nếu có mật khẩu thì điền ở đây
+$host = 'sql302.infinityfree.com';
+$db   = 'if0_40653409_guestbook_db';
+$user = 'if0_40653409';
+$pass = 'FV4SEsh1nhz'; // nếu có mật khẩu thì điền ở đây
 $charset = 'utf8mb4';
 
 $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
